cb2021a90b70f3d81f2e90e9da82e396c3fbbc4b
scherenhaenden <scherenhaenden@gmail.com>
2023-05-16 13:46:45 +0200

    Add more

--- MySQL/Test_UNI_20230516.sql
+++ MySQL/Test_UNI_20230516.sql
@@ -58,7 +58,7 @@ FOREIGN KEY (CreatedByUser)
 REFERENCES Users (Id);
 
 
-# Insertar datos en la tabla Users
+# Insert data into Users
 INSERT INTO Users (FirstName, LastName, Password, LastLogin, Created, Updated, Email, VerifiedAccount)
 VALUES ('Carlos' , 'Perencejo', 'Somepassword', null, null, null, 'someemail@email.com', false);
 
@@ -109,7 +109,7 @@ END;
 
 */
 
-# Insertar datos en la tabla Article
+# Insert data into Article
 # it should fail cuz of the foreign key constraint
 INSERT INTO Article (Title, Content, Created, Updated, CreatedByUser)
 VALUES ('Some title', 'Some content', now(), now(), 2);
@@ -121,3 +121,82 @@ INSERT INTO Article (Title, Content, Created, Updated, CreatedByUser)
 # but better
 VALUES ('Some title', 'Some content', now(), now(),
 (SELECT Id FROM Users WHERE FirstName = 'Carlos' AND LastName = 'Perencejo'));
+
+# Select data from Article
+SELECT * FROM Article;
+
+# Select data from Article and Users
+SELECT * FROM Article
+INNER JOIN Users
+ON Article.CreatedByUser = Users.Id;
+
+# Select data from Article and Users with aliases but only some columns
+SELECT
+    Article.Id AS ArticleId, Article.Title, Article.Content, Article.Created, Article.Updated,
+Users.Id AS UserId, Users.FirstName, Users.LastName, Users.Email
+FROM Article
+INNER JOIN Users
+ON Article.CreatedByUser = Users.Id;
+
+
+# I need more users
+INSERT INTO Users (FirstName, LastName, Password, LastLogin, Created, Updated, Email, VerifiedAccount)
+VALUES
+    ('John', 'Doe', 'Password123', null, NOW(), NOW(), null, true),
+    ('Emma', 'Smith', 'SecretPass', null, NOW(), NOW(), null, false),
+    ('Michael', 'Johnson', 'Secure123', null, NOW(), NOW(), null, true),
+    ('Sophia', 'Brown', 'MyPassword', null, NOW(), NOW(), null, false),
+    ('Daniel', 'Garcia', 'TopSecret', null, NOW(), NOW(), null, true),
+    ('Olivia', 'Miller', 'Confidential', null, NOW(), NOW(), null, false),
+    ('William', 'Anderson', 'Password1234', null, NOW(), NOW(), null, true),
+    ('Isabella', 'Wilson', 'SecurePassword', null, NOW(), NOW(), null, false),
+    ('James', 'Taylor', 'ProtectedPass', null, NOW(), NOW(), null, true),
+    ('Mia', 'Martinez', 'Secret123', null, NOW(), NOW(), null, false),
+    ('Benjamin', 'Clark', 'MySecretPass', null, NOW(), NOW(), null, true),
+    ('Ava', 'Lopez', 'SuperSecret', null, NOW(), NOW(), null, false),
+    ('Henry', 'Harris', 'ConfidentialPass', null, NOW(), NOW(), null, true),
+    ('Emily', 'Young', 'HiddenPass', null, NOW(), NOW(), null, false),
+    ('Alexander', 'Lee', 'Protected123', null, NOW(), NOW(), null, true),
+    ('Sofia', 'Walker', 'MyConfidentialPass', null, NOW(), NOW(), null, false),
+    ('Jacob', 'Hall', 'TopSecret123', null, NOW(), NOW(), null, true),
+    ('Madison', 'Turner', 'MySecurePass', null, NOW(), NOW(), null, false),
+    ('Ethan', 'Hernandez', 'SecretPassword123', null, NOW(), NOW(), null, true);
+
+SELECT * FROM Users;
+
+
+# I need more articles
+INSERT INTO Article (Title, Content, Created, Updated, CreatedByUser)
+VALUES ((SELECT SUBSTRING(MD5(RAND()), 1, 10)), (SELECT SUBSTRING(MD5(RAND()), 1, 10)), now(), now(),
+(SELECT Id FROM Users ORDER BY RAND() LIMIT 1));
+
+
+
+SELECT
+    Article.Id AS ArticleId, Article.Title, Article.Content, Article.Created, Article.Updated,
+Users.Id AS UserId, Users.FirstName, Users.LastName, Users.Email
+FROM Article
+INNER JOIN Users
+ON Article.CreatedByUser = Users.Id;
+
+# how many articles were written by each user?
+SELECT Users.ID AS USERID, COUNT(*) AS COUNT FROM Users
+INNER JOIN Article
+ON Users.Id = Article.CreatedByUser
+GROUP BY Users.Id;
+
+# find users that did not write any articles
+SELECT Users.ID AS USERID, COUNT(Article.CreatedByUser) AS COUNT FROM Users
+LEFT JOIN Article
+ON Users.Id = Article.CreatedByUser
+WHERE 1=1
+#AND Article.Id IS NULL
+GROUP BY Users.Id;
+
+SELECT * FROM Users
+WHERE Id NOT IN (SELECT CreatedByUser FROM Article);
+
+SELECT * FROM Article
+WHERE 1=1
+ORDER BY CreatedByUser ASC;
+
